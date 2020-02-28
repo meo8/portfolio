dadd5441511a59ae7871aa20b954df843394f2b0
Meo Huynh <meoh92@gmail.com>
2020-02-28 09:12:52 -0800

    Add JS for scroll-behavior

---
+++ main.js
@@ -0,0 +1,25 @@
+const homeIcon = document.getElementById("homeIcon");
+
+function smoothScroll(e) {
+  // if it wasn't for the preventDefault, the scroll wouldn't work
+  e.preventDefault();
+  window.scroll({
+    top: 2500,
+    left: 0,
+    behavior: 'smooth'
+  });
+
+  // Scroll certain amounts from current position
+  window.scrollBy({
+    top: 100, // could be negative value
+    left: 0,
+    behavior: 'smooth'
+  });
+
+  // Scroll to a certain element
+  document.querySelector('#intro').scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+
+homeIcon.addEventListener("click", smoothScroll);
