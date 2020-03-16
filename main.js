0e647f6f7eb59f1a812b348ff2edab6ea560b46d
Meo Huynh <meoh92@gmail.com>
2020-03-15 21:56:34 -0700

    Add active class to designated pages

--- main.js
+++ main.js
@@ -23,3 +23,14 @@ function smoothScroll(e) {
 }
 
 homeIcon.addEventListener("click", smoothScroll);
+
+// toggle nav-items for active class
+// const navItems = document.getElementsByClassName("nav-item");
+//
+// for (var i = 0; i < navItems.length; i++) {
+//   navItems[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
